e4506887578d2111925d0431d57e417bcb31c08f
Umang <umang8864gupta@gmail.com>
2022-08-10 12:57:17 +0530

    token deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TokenComponent } from './token/token.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TokenComponent
+    
   ],
   imports: [
     BrowserModule,
